7b170f10f08ad9e2b6e3b6877fedd7b35d641a77
hiha4649 <hiha.ha.ani.4649@gmail.com>
2023-09-06 18:31:46 +0900

    first commit

---
+++ miku.js
@@ -0,0 +1,8 @@
+//音量ボタンの制御
+const volumeButton = document.getElementsByClassName('volume');
+volumeButton.onclick = function() {
+  const volumeSize = false
+  if (volumeSize = false) {
+    
+  }
+}
